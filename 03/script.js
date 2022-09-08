ee986643703b0e0842b9d0aca09474ab87f8ecc2
Ryan Malm <ryan.malm@us.af.mil>
2022-09-08 14:43:11 -0500

    init commit

---
+++ 03/script.js
@@ -0,0 +1,17 @@
+const canvas = document.querySelector('.webgl');
+const scene = new THREE.Scene();
+//Red Cube
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({ color: '#EE0000'});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
