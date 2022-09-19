fe5d4fcde5648d5bb960ed6b5e6b5ef7ed533b3a
Ryan Malm <ryan.malm@us.af.mil>
2022-09-19 16:10:52 -0500

    progress made, starting lesson 11

--- 03-hello-three_js/03.js
+++ 03-hello-three_js/03.js
@@ -6,8 +6,8 @@ const material = new THREE.MeshBasicMaterial({ color: '#EE0000'});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 4;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,10 +1,24 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+console.log(dat)
+//tweakable parameters
+const parameters = {
+    color: 0xff0000,
+    spin: () =>
+    {
+        gsap.to(group.rotation, { duration: 1, y: group.rotation.y + Math.PI * 2 })
+    }
+}
 
 //scene
 const scene = new THREE.Scene()
-
+const canvas = document.querySelector('canvas')
+//materials
+const material = new THREE.MeshBasicMaterial({color: 0xFF0000, wireframe: true})
 //objects
 const group = new THREE.Group()
 group.scale.y = 2
@@ -12,40 +26,66 @@ group.rotation.y = 0.2
 scene.add(group)
 
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({ color: 0x008800})
-    )
+    new THREE.BoxGeometry(1,1,1, 2, 2, 2),
+    material
+)
 cube1.position.x = -1.5
 group.add(cube1)
 
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({ color: 0x000099})
+//bufferGeometry
+const geometry = new THREE.BufferGeometry()
+const count = 50
+const positionsArray = new Float32Array(count * 3 * 3)
+for (let i = 0; i < count * 3 * 3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+const triangle = new THREE.Mesh(
+    geometry,
+    material
+  
 )
-cube2.position.x = 0
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({ color: 0x440088})
-)
-cube3.position.x = -0.2
-cube3.position.y = 0.25
-cube3.position.z = 0.4
-group.add(cube3)
-
+group.add(triangle)
 
 //axes helper
 const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
+//sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+window.addEventListener('dblclick', () => {
+    if(!fullscreenElement){
+        canvas.requestFullscreen()
+        console.log('go fullscreen')
+    }else {
+        document.exitFullscreen()
+        console.log('leave fullscreen')
+    }
+})
+
+window.addEventListener('resize', () => {
+    //update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    //
+    //update tamera aspect
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 //camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height)
+const aspectRatio = sizes.width / sizes.height
+//widthconst camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
 camera.position.z = 3
 camera.position.y = 0.2
 camera.lookAt(group.position)
@@ -53,21 +93,50 @@ scene.add(camera)
 
 //renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const clock = new THREE.Clock()
 
+//cursor
+const cursor = {
+    x: 0, y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -( event.clientY / sizes.height - 0.5 )
+    //console.log(cursor.x, cursor.y)
+})
+
+//OrbitControls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+//debug
+const gui = new dat.GUI()
+gui.add(group.position, 'y').min(-3).max(3).step(0.01).name('elevation')
+gui.add(group, 'visible')
+gui.add(material, 'wireframe')
+gui.addColor(parameters, 'color').onChange(() =>
+{
+    material.color.set(parameters.color)
+})
+gui.add(parameters, 'spin')
+
+
 //animate
-gsap.to(group.position, {duration: 1, delay: 1, x: 2})
+//gsap.to(group.position, {duration: 1, delay: 1, x: 2})
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-    camera.position.x = Math.sin(elapsedTime)
-    camera.position.y = Math.cos(elapsedTime)
-    camera.lookAt(group.position)
+    //camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    //camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    //camera.position.y = cursor.y * 5
+    //camera.lookAt(group.position)
     renderer.render(scene, camera)
+    controls.update()
     window.requestAnimationFrame(tick)
 }   
 tick()
